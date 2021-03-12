1c53c4532e9d764f761e9084e7756e2b9eceda80
shardul <shardul.aeer@somaiya.edu>
2021-03-12 22:31:58 +0530

    feat: Read number from query params

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,25 @@ import twilio from 'twilio'
 require('dotenv').config()
 
 const app = express()
-
-const accountSid = process.env.TWILIO_ACCOUNT_SID!!
+const accountSid = process.env.TWILIO_ACCOUNT_SID!
 console.log('Account ID', accountSid)
-const authToken = process.env.TWILIO_AUTH_TOKEN!!
+const authToken = process.env.TWILIO_AUTH_TOKEN!
 const client = twilio(accountSid, authToken)
 
 app.post('/auth', async (req, res) => {
     try {
-        const verification = await client.verify.services(process.env.SERVICE_ID!!)
-            .verifications
-            .create({ to: '+919619477301', channel: 'sms' })
+        const { phoneNumber } = req.query
+        console.log('Phone number', phoneNumber)
+        if(phoneNumber && typeof(phoneNumber) == 'string') {
+            const verification = await client.verify.services(process.env.SERVICE_ID!)
+                .verifications
+                .create({ to: phoneNumber, channel: 'sms' })
 
-        console.log('Verification status', verification.status)
-        res.send(verification.status)
+            console.log('Verification status', verification.status)
+            res.send(verification.status)
+        } else {
+            res.sendStatus(422)
+        }
 
     } catch(error) {
         res.send(error)
