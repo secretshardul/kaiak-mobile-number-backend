085c070da0cdb29d30cdfe87f3ef806e0b6d6ebf
shardul <shardul.aeer@somaiya.edu>
2021-03-12 19:04:16 +0530

    refactor: Remove unnecessary code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import './pre-start'; // Must be the first import
-import app from '@server';
-import logger from '@shared/Logger';
+import express from 'express'
 
+const app = express()
 
-// Start the server
-const port = Number(process.env.PORT || 3000);
+app.post('/auth', (req, res) => {
+    res.send('ok')
+})
+
+const port = Number(process.env.PORT || 3000)
 app.listen(port, () => {
-    logger.info('Express server started on port: ' + port);
+    console.log('Express server started on port: ' + port);
 });
