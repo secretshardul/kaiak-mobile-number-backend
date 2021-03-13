ceb3e0a28cb0f82490f15d1fba9fa01fdab4952c
shardul <shardul.aeer@somaiya.edu>
2021-03-13 18:15:12 +0530

    feat: Smart contract access control
    Throw error if sender is not contract owner, when saving new addresses

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,12 +1,20 @@
-import { logging, PersistentMap } from 'near-sdk-as'
+import { logging, PersistentMap, context } from 'near-sdk-as'
 
-let map = new PersistentMap<string, string>("nanoAddressMap")
+const map = new PersistentMap<string, string>("nanoAddressMap")
+const NOT_OWNER_ERROR = 'Permission denied, sender must be the owner'
 
 export function getNanoAddress(mobileNumber: string): string | null {
   return map.get(mobileNumber)
 }
 
 export function setNanoAddress(mobileNumber: string, nanoAddress: string): void {
+  const sender = context.sender
+  const contractName = context.contractName
+  logging.log(
+    'Sender: "' + sender + '", contract address: "' + contractName + '"'
+  )
+  assert(sender == contractName, NOT_OWNER_ERROR)
+
   logging.log(
     'Saving address "' + nanoAddress + '" for number "' + mobileNumber + '"'
   )
